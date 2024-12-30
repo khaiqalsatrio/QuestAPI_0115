f572989e73bad3babe6d8d65719c3c51006d152e
Khaiqol <Khaiqol@gmail.com>
2024-12-30 12:20:11 +0700

    menambahkan fun update body di update view

--- app/src/main/java/com/example/project_11/ui/View/UpdateView.kt
+++ app/src/main/java/com/example/project_11/ui/View/UpdateView.kt
@@ -1,11 +1,16 @@
 package com.example.project_11.ui.View
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -13,10 +18,13 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.project_11.ui.costumWidget.CostumeTopAppBar
 import com.example.project_11.ui.navigation.DestinasiNavigasi
 import com.example.project_11.ui.viewModel.PenyediaViewModel
+import com.example.project_11.ui.viewModel.UpdateUiEvent
+import com.example.project_11.ui.viewModel.UpdateUiState
 import com.example.project_11.ui.viewModel.UpdateViewModel
 import kotlinx.coroutines.launch
 
@@ -65,5 +73,31 @@ fun UpdateView(
                 .verticalScroll(rememberScrollState())
                 .fillMaxWidth()
         )
+    }
+}
+
+@Composable
+fun UpdateBody(
+    updateUiState: UpdateUiState,
+    onSiswaValueChange: (UpdateUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ) {
+        FormInput(
+            UpdateUiEvent = updateUiState.updateUiEvent,
+            onValueChange = onSiswaValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Update")
+        }
     }
 }
