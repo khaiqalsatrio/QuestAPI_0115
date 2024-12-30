b2208535de06766b4ad4e6ea2b667b2b42521c4a
Khaiqol <Khaiqol@gmail.com>
2024-12-30 11:46:28 +0700

    membuat fun detail view

---
+++ app/src/main/java/com/example/project_11/ui/View/DetailView.kt
@@ -0,0 +1,98 @@
+package com.example.project_11.ui.View
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.project_11.ui.costumWidget.CostumeTopAppBar
+import com.example.project_11.ui.navigation.DestinasiNavigasi
+import com.example.project_11.ui.viewModel.DetailUiState
+import com.example.project_11.ui.viewModel.DetailViewModel
+import com.example.project_11.ui.viewModel.PenyediaViewModel
+import java.lang.reflect.Modifier
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "detail"
+    override val titleRes = "Detail Mahasiswa"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailView(
+    nim: String,
+    navigateBack: () -> Unit,
+    onClick: () -> Unit,
+    viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val uiState by viewModel.uiState.collectAsState()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    viewModel.getMahasiswaById(nim)
+
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiDetail.titleRes,
+                canNavigateBack = true,
+                navigateUp = navigateBack,
+                scrollBehavior = scrollBehavior,
+                onRefresh = {
+                    viewModel.getMahasiswaById(nim)
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onClick,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(imageVector = Icons.Default.Edit, contentDescription = "Edit Mahasiswa")
+            }
+        },
+    ) { innerPadding ->
+        when (uiState) {
+            is DetailUiState.Loading -> {
+                Text("Loading...", Modifier.fillMaxWidth())
+            }
+            is DetailUiState.Success -> {
+                val mahasiswa = (uiState as DetailUiState.Success).mahasiswa
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(innerPadding)
+                        .padding(16.dp),
+                    verticalArrangement = Arrangement.spacedBy(12.dp),
+                    horizontalAlignment = Alignment.Start
+                ) {
+                    ItemDetailMhs(mahasiswa = mahasiswa)
+                }
+            }
+            is DetailUiState.Error -> {
+                Text(
+                    text = "Gagal Menyinpan Data",
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.error
+                )
+            }
+        }
+    }
+}
+
